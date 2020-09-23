ae3c78cf00f2bfdf9bb4aa84b2971cfb3331e512
tonytan4ever <tonytan198211@gmail.com>
2020-09-23 00:44:53 -0400

    Add set_table_max_id seq function

--- SQL/postgres/handle_duplicates.py
+++ SQL/postgres/handle_duplicates.py
@@ -3,8 +3,8 @@ from jinja2 import Template
 
 upsert_tpl_string = '''
 begin;
-create temp table {{ table_name }}_clone on commit drop as select * from {{ table_name }} with no data;
-\copy {{ table_name }}_clone FROM '{{ csv_dump_path }}';
+create temp table {{ table_name }}_clone as select * from {{ table_name }} with no data;
+\copy {{ table_name }}_clone FROM '{{ csv_dump_path }}' delimiter ',' csv header;
 insert into {{ table_name }} select  * from {{ table_name }}_clone on conflict do nothing;
 commit;
 '''
@@ -13,6 +13,9 @@ dump_csv_string = '''
     \copy {{ table_name }} TO '{{ csv_dump_path }}' delimiter ',' csv header;
 '''
 
+set_table_max_id_seq_string = '''
+    select setval('{{ table_name }}_id_seq', (select MAX(id) from {{ table_name }));
+'''
 
 def get_upsert_transaction(table_name, csv_dump_path):
     return Template(upsert_tpl_string).render(
@@ -21,8 +24,13 @@ def get_upsert_transaction(table_name, csv_dump_path):
     )
 
 
-def get_dump_csv_transaction(table_name, csv_dump_path):
+def get_dump_csv_stmt(table_name, csv_dump_path):
     return Template(dump_csv_string).render(
         table_name = table_name,
         csv_dump_path = csv_dump_path
+    )
+    
+def get_set_table_max_id_seq_stmt(table_name):
+    return Template(set_table_max_id_seq_string).render(
+        table_name = table_name
     )
